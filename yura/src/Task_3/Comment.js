8a1d565f6739626e53945fc41a4c63d6b6b9b04a
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-16 09:13:48 +0300

    Created init state in CommentList.js. Created comment.js

---
+++ yura/src/Task_3/Comment.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+function Comment({}) {
+  return (
+    <div className="comment">
+      <div className="card text-white bg-dark mb-3">
+        <div className="card-header"></div>
+        <div className="card-body">
+          <h5 className="card-title">Your comment</h5>
+          <div className="card-text"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
--- yura/src/Task_3/CommentsList.js
+++ yura/src/Task_3/CommentsList.js
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
+import Comment from "./Comment";
 function CommentsList() {
-  <div></div>;
+  const [allComment, setAllComment] = useState([
+    {
+      id: Date.now(),
+      dateCreate: new Date().toLocaleString(),
+      like: 0,
+      dislike: 0,
+      text: `Судьба шепчет воину, что приближается буря, воин шепчет в ответ: "Я и есть буря..."`,
+    },
+    {
+      id: Date.now() + 1,
+      dateCreate: new Date().toLocaleString(),
+      like: 0,
+      dislike: 0,
+      text: `Руки работают видят глаза, пархай как бабочка жаль как пчела "Мухамед Али"`,
+    },
+  ]);
+
+  return (
+    <div>
+      <Comment />
+    </div>
+  );
 }
 
 export default CommentsList;
