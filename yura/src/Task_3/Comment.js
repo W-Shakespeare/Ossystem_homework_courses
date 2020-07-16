283c2ea889050bca929d4ccf3b3602d3029a5bd0
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-16 09:21:46 +0300

    Npm install five icons from material-ui/icons then import icons in Comment.js

--- yura/src/Task_3/Comment.js
+++ yura/src/Task_3/Comment.js
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
+import ThumbDownAltIcon from "@material-ui/icons/ThumbDownAlt";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import CloseIcon from "@material-ui/icons/Close";
+import EditIcon from "@material-ui/icons/Edit";
+import SaveIcon from "@material-ui/icons/Save";
 function Comment({}) {
   return (
     <div className="comment">
       <div className="card text-white bg-dark mb-3">
-        <div className="card-header"></div>
+        <div className="card-header">
+          {dateCreate}
+          <CloseIcon />
+        </div>
         <div className="card-body">
           <h5 className="card-title">Your comment</h5>
-          <div className="card-text"></div>
+          <div className="card-text">{text}</div>
+          <div className="editIcon">
+            <div className="editIcon-thumbUpIcon editIcon-allIcons">
+              <ThumbUpIcon color="primary" />
+              <p className="p-like">{like}</p>
+            </div>
+            <div className="editIcon-editIcon editIcon-allIcons">
+              <EditIcon />
+            </div>
+            <div className="editIcon-thumbDownAltIcon editIcon-allIcons">
+              <ThumbDownAltIcon color="secondary" />
+              <p className="p-dislike">{dislike}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
